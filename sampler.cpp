d6b651f7284d7666c132affcbf0f2dbe1e823e5a
Luciano Siqueira <lcnsqr@gmail.com>
2021-12-22 23:23:52 -0300

    Suprimir sampler

--- sampler.cpp
+++ sampler.cpp
@@ -8,7 +8,7 @@ Sampler::Sampler(Therm* th, int pin, unsigned long wait): Task(wait), th(th), pi
 
 void Sampler::action(){
   int input = analogRead(pin);
-  Serial.print(input);
-  Serial.print("	");
-  Serial.println(th->celsius((double)input));
+  //Serial.print(input);
+  //Serial.print("	");
+  //Serial.println(th->celsius((double)input));
 }
