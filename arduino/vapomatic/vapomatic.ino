92648c876ed5282ea1d4541e83ba05d495a5aa63
Luciano <lcnsqr@gmail.com>
2023-02-05 06:08:38 -0300

    Retomar setup UIs

--- arduino/vapomatic/vapomatic.ino
+++ arduino/vapomatic/vapomatic.ino
@@ -21,9 +21,9 @@ Therm therm(&session, A0, Therm::bufLen * 17);
 U8G2_SH1106_128X64_NONAME_2_HW_I2C display(U8G2_R2, U8X8_PIN_NONE);
 // Telas da UI
 scrMain uiMain(&session, &display);
-//scrSetup uiSetup(&session, &display);
-//scrCalib uiCalib(&session, &display);
-//scrPID uiPID(&session, &display);
+scrSetup uiSetup(&session, &display);
+scrCalib uiCalib(&session, &display);
+scrPID uiPID(&session, &display);
 // Monitoramento de eventos
 Monitor monitor(&session, &uiMain, 4, 8, 25);
 // Callback de eventos do rotary encoder
