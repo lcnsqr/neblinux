d5f7765c22906eb5469c07f9255ac2d8ab2eea80
Luciano <lcnsqr@gmail.com>
2024-10-02 15:56:56 -0300

    Descarte do utilitário antigo

--- setup/gui.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "\nIniciando GUI..."
-cd gui
-exec node server.js
--- setup/gui/wasm/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export EMSDK_QUIET=1
-source emsdk/emsdk_env.sh
-emcc mat.c -s WASM=1 -s EXPORTED_FUNCTIONS="['_malloc', '_free']" -o mat.js -O1
